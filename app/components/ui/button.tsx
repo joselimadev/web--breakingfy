8cf1cbd8935c2f4eee87d5ec509e61c7c88a401e
Jose Lima <jcolscript@gmail.com>
2023-12-03 00:24:31 -0300

    feat: create ui components

---
+++ app/components/ui/button.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+interface ButtonProps {
+  label?: string;
+  type?: 'button' | 'submit' | 'reset';
+  disabled?: boolean;
+  outline?: boolean;
+  small?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  disabled,
+  outline,
+  small,
+  type = 'button',
+}) => {
+  return (
+    <button
+      disabled={disabled}
+      type={type}
+      className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 
+        transition w-full bg-green-800 text-white outline-none py-4 text-lg font-semibold
+      `}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
---
+++ app/components/ui/input.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
+
+interface InputProps {
+  id: string;
+  label: string;
+  type?: string;
+  placeholder?: string;
+  disabled?: boolean;
+  formatPrice?: boolean;
+  required?: boolean;
+  register: any;
+  errors?: FieldErrors;
+}
+
+const Input: React.FC<InputProps> = ({
+  id,
+  label,
+  type = 'text',
+  disabled,
+  formatPrice,
+  required,
+  register,
+  errors,
+}) => {
+  return (
+    <div className="w-full relative">
+      <input
+        id={id}
+        {...register(id, { required })}
+        type={type}
+        placeholder={' '}
+        disabled={disabled}
+        className={`
+          peer 
+          w-full 
+          p-4
+          pt-6 
+          font-light 
+          bg-white 
+          border-2 
+          rounded-md 
+          outline-none 
+          trasition 
+          disabled:opacity-70 
+          default:cursor-not-allowed
+          ${formatPrice ? `pl-9` : `pl-4`}
+          ${errors && errors[id] ? `border-rose-500` : `border-neutral-300`}
+          ${
+            errors && errors[id]
+              ? `focus:border-rose-500`
+              : `focus:border-green-800`
+          }
+        `}
+      />
+      <label
+        htmlFor={id}
+        className={`
+          absolute 
+          text-md
+          duration-150
+          transform
+          -translate-y-5
+          scale-75
+          top-6
+          z-10
+          origin-[0]
+          left-4
+          peer-placeholder-shown:scale-100
+          peer-placeholder-shown:translate-y-0
+          peer-focus:scale-75
+          peer-focus:-translate-y-5
+          ${errors && errors[id] ? `text-rose-500` : `text-zinc-700`}
+        `}
+      >
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default Input;
